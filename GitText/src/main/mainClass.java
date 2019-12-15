1951f9017fc9c8ec97aad0d501b9d40e7f8f8e3a
LeeSangYoeb <xyrho.v@gmail.com>
2019-12-15 15:34:02 +0900

    test
    파일 추가 테스트

--- GitText/src/main/mainClass.java
+++ GitText/src/main/mainClass.java
@@ -1,9 +1,14 @@
 package main;
 
+import view.MainViewer;
+
 public class mainClass {
 	public static void main(String[] args) {
 		
 		System.out.println("sang yoeb git teat this one");
+		
+		
+		MainViewer as = new MainViewer();
 		
 	}
 }
---
+++ GitText/src/view/MainViewer.java
@@ -0,0 +1,10 @@
+package view;
+
+public class MainViewer {
+
+	public MainViewer() {
+
+	 System.out.println("main viewer");
+	}
+	
+}
